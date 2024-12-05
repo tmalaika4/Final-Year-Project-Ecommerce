9e8bfa34dee2a461d297250c7b4b5a83a49d3649
malaikatabassum <141026533+tmalaika4@users.noreply.github.com>
2024-12-05 13:06:43 +0500

    render urls

--- E-commerce/backend/index.js
+++ E-commerce/backend/index.js
@@ -83,7 +83,7 @@ app.post("/upload",upload.single('product'),(req, res) =>{
 
     res.json({
         success:1,
-        image_url:`http://localhost:${port}/images/${req.file.filename}`
+        image_url:`https://final-year-project-ecommerce.onrender.com/images/${req.file.filename}`
     })
 })
 
