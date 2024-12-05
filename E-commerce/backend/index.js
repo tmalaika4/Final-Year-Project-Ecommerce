b248d3581f1a0149653c29013907c5e59805804d
tmalaika5 <malaikatabassum001@gmail.com>
2024-12-05 20:02:55 +0500

    vercel changes

--- E-commerce/backend/index.js
+++ E-commerce/backend/index.js
@@ -83,7 +83,7 @@ app.post("/upload",upload.single('product'),(req, res) =>{
 
     res.json({
         success:1,
-        image_url:`http://localhost:4000/images/${req.file.filename}`
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
     })
 })
 
